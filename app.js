2243f2ccfc7a677526e3d5c324f26c2cb314f6ea
Neo <neovichnn@gmail.com>
2019-04-04 15:16:09 +0300

    init

---
+++ app.js
@@ -0,0 +1,46 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+const cors = require('cors')
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors())
+
+app.use('/v1.0/clinets/all', require('./routes/v1.js'))
+app.use('/v1.0/clinets/all', require('./routes/v2.js'))
+app.use('/v1.0/clinets/all', require('./routes/v3.js'))
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+const server = require('http').Server(app)
+
+module.exports = { app, server };
