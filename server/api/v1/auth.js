ba58deefb4ff7d929e7f5cf8f379da296979820f
Sergey Romanov <serhioromano@outlook.com>
2019-05-21 08:51:22 -0700

    Format files

--- server/api/v1/auth.js
+++ server/api/v1/auth.js
@@ -1,13 +1,13 @@
 const Config = require('config')
 
 module.exports = {
-  run: function (req, res, _data) {
-    return new Promise((resolve, reject) => {
-      let result = { error: 403, message: "Not authorized!" };
-      if (Config.ADMIN.login == _data.login && Config.ADMIN.pass == _data.pass) {
-        result = { error: 0, result: 1 };
-      }
-       resolve(result);
-    });
-  }
+    run: function (req, res, _data) {
+        return new Promise((resolve, reject) => {
+            let result = { error: 403, message: "Not authorized!" };
+            if (Config.ADMIN.login == _data.login && Config.ADMIN.pass == _data.pass) {
+                result = { error: 0, result: 1 };
+            }
+            resolve(result);
+        });
+    }
 }
--- server/api/v1/clients-all.js
+++ server/api/v1/clients-all.js
@@ -1,15 +1,15 @@
 const AwsDB = require("../../lib/AwsDB");
 
 module.exports = {
-  run: function (req, res, _data) {
-    return new Promise((resolve, reject) => {
-      AwsDB.docClient.scan({ TableName: "clients" }, function (err, data) {
-        if (err) {
-          resolve({ error: 100, message: err.message });
-        } else {
-          resolve(data);
-        }
-      });
-    });
-  }
+    run: function (req, res, _data) {
+        return new Promise((resolve, reject) => {
+            AwsDB.docClient.scan({ TableName: "clients" }, function (err, data) {
+                if (err) {
+                    resolve({ error: 100, message: err.message });
+                } else {
+                    resolve(data);
+                }
+            });
+        });
+    }
 };
